c911c8db117e6fbf37d5bd3fbe5ac4b92b5e6f93
Eman Ayyad <eman.khalifa@squadio.com>
2025-12-04 15:02:57 +0200

    add gitignore, caching using redis and connect to mongodb

---
+++ services/cache.js
@@ -0,0 +1,32 @@
+const redis = require("redis");
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient({ url: redisUrl });
+
+const cacheMiddleware = (duration = 3600) => {
+  return (req, res, next) => {
+    if (req.method !== "GET") {
+      return next();
+    }
+
+    const key = "__express__" + (req.originalUrl || req.url);
+
+    client.get(key, (err, data) => {
+      if (data) {
+        res.set("X-Cache", "HIT");
+        res.send(JSON.parse(data));
+      } else {
+        res.set("X-Cache", "MISS");
+        const originalSend = res.send;
+
+        res.send = function (data) {
+          client.set(key, JSON.stringify(data), { EX: duration });
+          originalSend.call(this, data);
+        };
+
+        next();
+      }
+    });
+  };
+};
+
+module.exports = cacheMiddleware;
